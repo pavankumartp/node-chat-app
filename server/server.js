ef85b2ac7614def59471be142e3412d36cf813a8
Pavan Kumar <pavan.kumar@sap.com>
2017-10-03 22:21:15 +0530

    connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,13 +1,28 @@
 const path = require('path');
 var express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 
 var app = express();
+var server= http.createServer(app);
+var io = socketIO(server);
+
 const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT||3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection', (socket)=>{
+  console.log('new connection established');
+  socket.on('disconnect', ()=>{
+    console.log('client disconnected');
+  })
+});
+
+
+
+
+server.listen(port,()=>{
       console.log(`server started on port ${port}`);
     });
